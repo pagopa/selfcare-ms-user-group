edcb7a7b08fcd37234b2fe08eb916191e2418d6c
USERSAD\bgalgamu <bgalgamu@emeal.nttdata.com>
2022-03-09 14:54:47 +0100

    add exceptionHandler

--- web/src/main/java/it/pagopa/selfcare/user_group/web/handler/GroupExceptionHandler.java
+++ web/src/main/java/it/pagopa/selfcare/user_group/web/handler/GroupExceptionHandler.java
@@ -3,6 +3,7 @@ package it.pagopa.selfcare.user_group.web.handler;
 import it.pagopa.selfcare.commons.web.model.ErrorResource;
 import it.pagopa.selfcare.user_group.connector.exception.ResourceAlreadyExistsException;
 import it.pagopa.selfcare.user_group.connector.exception.ResourceNotFoundException;
+import it.pagopa.selfcare.user_group.connector.exception.ResourceUpdateException;
 import it.pagopa.selfcare.user_group.web.controller.GroupController;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
@@ -25,6 +26,13 @@ public class GroupExceptionHandler {
         return new ErrorResource(e.getMessage());
     }
 
+    @ExceptionHandler({ResourceUpdateException.class})
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ResponseBody
+    ErrorResource handleResourceUpdateException(ResourceUpdateException e) {
+        log.warn(UNHANDLED_EXCEPTION, e);
+        return new ErrorResource(e.getMessage());
+    }
 
     @ExceptionHandler({ResourceNotFoundException.class})
     @ResponseStatus(HttpStatus.NOT_FOUND)
