871587546824b3bd1ff551e09c120ac7851292a4
USERSAD\bgalgamu <bgalgamu@emeal.nttdata.com>
2022-03-15 11:12:18 +0100

    rework addMember API

--- web/src/main/java/it/pagopa/selfcare/user_group/web/controller/GroupController.java
+++ web/src/main/java/it/pagopa/selfcare/user_group/web/controller/GroupController.java
@@ -6,7 +6,6 @@ import io.swagger.annotations.ApiParam;
 import it.pagopa.selfcare.user_group.connector.api.UserGroupOperations;
 import it.pagopa.selfcare.user_group.core.UserGroupService;
 import it.pagopa.selfcare.user_group.web.model.CreateUserGroupDto;
-import it.pagopa.selfcare.user_group.web.model.MemberUUID;
 import it.pagopa.selfcare.user_group.web.model.UpdateUserGroupDto;
 import it.pagopa.selfcare.user_group.web.model.UserGroupResource;
 import it.pagopa.selfcare.user_group.web.model.mapper.GroupMapper;
@@ -106,18 +105,18 @@ public class GroupController {
         return result;
     }
 
-    @PatchMapping(value = "/{id}/members")
+    @PutMapping(value = "/{id}/members/{userId}")
     @ResponseStatus(HttpStatus.OK)
     @ApiOperation(value = "", notes = "${swagger.user-group.groups.api.addMember}")
     public void addMemberToUserGroup(@ApiParam("${swagger.user-group.model.id}")
                                      @PathVariable("id")
                                              String id,
-                                     @RequestBody
-                                     @Valid
-                                             MemberUUID member) {
+                                     @ApiParam("${swagger.dashboard.user.model.id}")
+                                     @PathVariable("userId")
+                                             String userId) {
         log.trace("addMemberToUserGroup start");
         log.debug("addMemberToUserGroup id = {}", id);
-        groupService.addMember(id, member.getMember());
+//        groupService.addMember(id, member.getMember());
         log.trace("addMemberToUserGroup end");
     }
 
