7615499dc49cd7c722711a52e8a977c4def41e69
pierpaolo.didato@emeal.nttdata.com <Aiap1955?^@#>
2024-01-15 10:51:25 +0100

    Updates

--- web/src/main/java/it/pagopa/selfcare/user_group/web/model/UserGroupResource.java
+++ web/src/main/java/it/pagopa/selfcare/user_group/web/model/UserGroupResource.java
@@ -45,9 +45,6 @@ public class UserGroupResource {
     @ApiModelProperty(value = "${swagger.user-group.model.createdBy}")
     private String createdBy;
 
-    @ApiModelProperty(value = "${swagger.user-group.model.modifiedAt}")
-    private Instant modifiedAt;
-
     @ApiModelProperty(value = "${swagger.user-group.model.modifiedBy}")
     private String modifiedBy;
 
